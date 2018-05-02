6d0820bccf8576a32f54294a13e0602d69c886f1
Anthony Kramer <anthony.kramer@gmail.com>
2018-05-02 09:05:03 -0700

    updated slack_alerts eith new alerts, renamed logArgs

--- activePlugins/record_user_login.py
+++ activePlugins/record_user_login.py
@@ -13,9 +13,12 @@ import logging
 
 def registerCallbacks(reg):
     """
-    Register all necessary or appropriate callbacks for this plugin.
+    Register our callbacks.
+
+    :param reg: A Registrar instance provided by the event loop handler.
     """
-    # eventFilter = {'Shotgun_Task_Change': ['sg_status_list']}
+
+    # filter for login events and register the callback
     eventFilter = {'Shotgun_User_Login': None}
     reg.registerCallback(
         os.environ["SGDAEMON_RECORDLOGIN_NAME"],
@@ -25,24 +28,24 @@ def registerCallbacks(reg):
         None,
     )
 
-    # Set the logging level for this particular plugin. Let debug and above
-    # messages through (don't block info, etc). This is particularly usefull
-    # for enabling and disabling debugging on a per plugin basis.
     reg.logger.setLevel(logging.DEBUG)
     reg.logger.debug("Registered callback.")
 
 
 def record_login(sg, logger, event, args):
     """
-    A callback that logs its arguments.
+    A callback that records the users login datetime to thier user record on
+    in shotgun.
 
     :param sg: Shotgun API handle.
     :param logger: Logger instance.
     :param event: A Shotgun EventLogEntry entity dictionary.
     :param args: Any additional misc arguments passed through this plugin.
     """
-    # logger.info("%s" % str(event))
+    logger.debug("%s" % str(event))
+    user_name = event["entity"]["name"]
     user_id = event["entity"]["id"]
     login_time = event["created_at"]
 
     sg.update("HumanUser", user_id, {"sg_last_login": login_time})
+    logger.debug("Recorded user login datetime for user %s" % user_name)
